917e2ccd09016a49d3b4473ca89ca9c934eebb96
Andreas Jaggi <x-way@waterwave.ch>
2025-04-20 21:36:20 +0200

    Make retries configurable to work around the new 10req/minute ratelimit

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -5,12 +5,14 @@ package provider
 	```
 	provider "uptimerobot" {
 	  api_key = "[YOUR MAIN API KEY]"
+	  retries = 10
 	}
 	```
 */
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	uptimerobotapi "github.com/vexxhost/terraform-provider-uptimerobot/internal/provider/api"
 )
@@ -19,10 +21,17 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"api_key": &schema.Schema{
+			"api_key": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("UPTIMEROBOT_API_KEY", nil),
+			},
+			"retries": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				DefaultFunc:  schema.EnvDefaultFunc("UPTIMEROBOT_RETRIES", 10),
+				Description:  "Maximum number of retries to perform when an API request fails. This can also be provided as an environment variable `UPTIMEROBOT_RETRIES`",
+				ValidateFunc: validation.IntBetween(0, 10000),
 			},
 		},
 		DataSourcesMap: map[string]*schema.Resource{
@@ -35,7 +44,7 @@ func Provider() *schema.Provider {
 			"uptimerobot_status_page":   resourceStatusPage(),
 		},
 		ConfigureFunc: func(r *schema.ResourceData) (interface{}, error) {
-			config := uptimerobotapi.New(r.Get("api_key").(string))
+			config := uptimerobotapi.New(r.Get("api_key").(string), r.Get("retries").(int))
 			return config, nil
 		},
 	}
